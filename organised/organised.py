ea962730d8c836f613b407387f7c3c7e64ce4fc8
Luke Plausin <luke.plausin@gmail.com>
2019-08-20 00:56:59 +0100

    Added some features - clean up junk files like .DS_Store

--- organised/organised.py
+++ organised/organised.py
@@ -8,14 +8,15 @@ import datetime
 import filecmp
 import logging
 import json
+import re
 
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 # Some hard coded logging stuff so I can see whats happening
 handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.INFO)
+handler.setLevel(logging.DEBUG)
 # formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 formatter = logging.Formatter('%(levelname)s - %(message)s')
 handler.setFormatter(formatter)
@@ -23,14 +24,18 @@ logger.addHandler(handler)
 
 
 preferences = {
-    "images": {
-        "extensions": ["jpg", "jpeg", "png"],
+    "camera": {
+        "extensions": [".jpg", ".jpeg", ".png", ".3gp", ".mov", ".mp4"],
         # "destination": "$HOME/Pictures/Camera/{Date:%Y-%b}/{Date:%Y-%m-%d} ({EXIF_Model})/{Date:%Y%m%d_%H%M%S}.{File_FileTypeExtension}"
         "destination": "$HOME/Pictures/Camera/{Date:%Y-%b} ({EXIF_Model})/{Date:%Y%m%d_%H%M%S}.{File_FileTypeExtension}"
     },
-    "video": {
-        "extensions": ["mov", "vid", "xvid"]
+    "junk": {
+        "extensions": [".tmp"],
+        "patterns": [".DS_Store"]
     },
+    # "movie": {
+    #     "extensions": ["mov", "vid", "xvid"]
+    # },
     "documents": {
         "extensions": ["doc", "docx", "csv", "txt"]
     }
@@ -70,8 +75,8 @@ def move_file(source, destination):
         logger.info("Removing empty directory {}".format(source_dirname))
         os.rmdir(source_dirname)
 
-def process_images(image_list):
-    # Process images
+def process_camera(image_list):
+    # Process images with exif metadata
 
     with exiftool.ExifTool() as et:
         metadata = et.get_metadata_batch(image_list)
@@ -95,38 +100,71 @@ def process_images(image_list):
             file_metadata['Date'] = date_parsed
 
             # metadata['Augmented:CreateDate'] = date
-            destination = preferences['images']['destination'].format(
+            destination = preferences['camera']['destination'].format(
                 **file_metadata
             )
-            logger.debug(json.dumps(file_metadata, default=str))
             move_file(file_metadata['SourceFile'], destination)
         except KeyError as e:
             logger.error("File {} missing key {}".format(file_metadata['SourceFile'], e))
+            logger.debug(json.dumps(file_metadata, default=str, indent=2))
         except Exception as e:
             logger.error("Exception: {}".format(e))
 
 
-def main(input_dir='/Users/lukeplausin/Desktop/organise_me/'):
+def process_junk(input_file_list):
+    for file in input_file_list:
+        logger.info("Removing junk file {}".format(file))
+        # os.remove(file)
+
+
+def cleanup_empty_dirs(input_dir):
+    # Remove empty directories in a tree
+    # for root, dirs, files in os.walk(input_dir):
+    #     if all([not os.path.isfile(f) for f in files]) and \
+    #             all([not os.path.isdir(d) for d in dirs]):
+    #         logger.info('Removing empty directory {}'.format(root))
+    #         os.rmdir(root)
+    pass
+
+
+def match_file_spec(spec, name):
+    name_noext, ext = os.path.splitext(name)
+    if ext in spec['extensions']:
+        return True
+    else:
+        for pattern in spec.get('patterns', []):
+            if re.match(pattern, name):
+                return True
+
+
+def main(input_dir='~/Desktop/organise_me/'):
+    input_dir = os.path.expanduser(os.path.expandvars(input_dir))
     for root, dirs, files in os.walk(input_dir):
         for name in files:
             # print(os.path.join(root, name))
+            file_type_recognised = False
             for filetype, filetype_data in preferences.items():
-                if name.split('.')[-1] in filetype_data['extensions']:
-                    # print("Found {} file {}".format(
-                    #     filetype, os.path.join(root, name)))
+                if match_file_spec(spec=filetype_data, name=name):
+                    file_type_recognised = True
                     file_list = preferences[filetype].get('files', [])
                     file_list.append(os.path.join(root, name))
                     preferences[filetype]['files'] = file_list
+
+            if not file_type_recognised:
+                logger.info("Unknown file type: {}".format(name))
         for name in dirs:
             pass
-        if all([not os.path.isfile(f) for f in files]) and \
-                all([not os.path.isdir(d) for d in dirs]):
-            logger.info('Removing empty directory {}'.format(root))
-            # os.rmdir(root)
-            # print("In directory {}".format(os.path.join(root, name)))
-    
+        
     # Process images
-    process_images(preferences['images'].get('files', []))
+    file_list = preferences['camera'].get('files', [])
+    if file_list:
+        process_camera(file_list)
+
+    # Remove junk
+    process_junk(preferences['junk'].get('files', []))
+
+    # Cleanup dirs
+    cleanup_empty_dirs(input_dir)
 
 
 if __name__ == '__main__':
